a8bbdf8a75308f2381aefb8ecf2e39c1ffccc888
jilherme <rosa.guilhermediogo@gmail.com>
2023-10-30 23:57:53 -0300

    add: redux actions and TS

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,13 +1,15 @@
-import { useSelector } from "react-redux";
+import { useAppSelector } from "../store";
 
 export function TodoList() {
-  const todos = useSelector(store => {
-    return store.todo
-  })
+  const todos = useAppSelector((store) => {
+    return store.todo;
+  });
 
   return (
     <ul>
-      {todos.map(todo => <li key={todo}>{todo}</li>)}
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
     </ul>
   );
 }
