1ee549cfa7b80fb4ec0cd61b3856ee711ce588da
jilherme <rosa.guilhermediogo@gmail.com>
2023-11-01 23:26:48 -0300

    add: Base UI structure

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { AddTodo } from "./components/AddTodo";
 import { TodoList } from "./components/TodoList";
 import { store } from "./store";
 
+import "./styles/global.css";
+
 export default function App() {
   return (
     <ReduxProvider store={store}>
