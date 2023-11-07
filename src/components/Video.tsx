d05129a069c16c10d1131526f33624282caf2a3a
jilherme <rosa.guilhermediogo@gmail.com>
2023-11-06 21:47:10 -0300

    add: autoplay config

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,8 +1,12 @@
 import ReactPlayer from "react-player"
 import { useAppSelector } from "../store"
+import { useDispatch } from "react-redux"
+import { next } from "../store/slices/player"
 
 export const Video = () => {
-  const video = useAppSelector((state) => {
+  const dispatch = useDispatch()
+
+  const lesson = useAppSelector((state) => {
     const { currentModuleIndex, currentLessonIndex } = state.player
 
     const currentLessonData =
@@ -13,13 +17,19 @@ export const Video = () => {
     return currentLessonData
   })
 
+  function handlePlayNext() {
+    dispatch(next())
+  }
+
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <ReactPlayer
         width="100%"
         height="100%"
         controls
-        url={`https://www.youtube.com/watch?v=${video.id}`}
+        playing
+        onEnded={handlePlayNext}
+        url={`https://www.youtube.com/watch?v=${lesson.id}`}
       />
     </div>
   )
