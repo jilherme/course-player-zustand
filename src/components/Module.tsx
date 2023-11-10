fd04f6d27d1da663b06889e01b77d02f0d2e3873
jilherme <rosa.guilhermediogo@gmail.com>
2023-11-10 00:18:18 -0300

    add: loading course data

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -22,7 +22,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
   })
 
   const lessons = useAppSelector(
-    (state) => state.player.course.modules[moduleIndex].lessons
+    (state) => state.player.course?.modules[moduleIndex].lessons
   )
 
   return (
@@ -42,21 +42,22 @@ export function Module({ moduleIndex, title, amountOfLessons }: ModuleProps) {
 
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson, lessonIndex) => {
-            const isCurrent =
-              currentModuleIndex === moduleIndex &&
-              currentLessonIndex === lessonIndex
+          {lessons &&
+            lessons.map((lesson, lessonIndex) => {
+              const isCurrent =
+                currentModuleIndex === moduleIndex &&
+                currentLessonIndex === lessonIndex
 
-            return (
-              <Lesson
-                key={lessonIndex}
-                title={lesson.title}
-                duration={lesson.duration}
-                isCurrent={isCurrent}
-                onPlay={() => dispatch(play({ moduleIndex, lessonIndex }))}
-              />
-            )
-          })}
+              return (
+                <Lesson
+                  key={lessonIndex}
+                  title={lesson.title}
+                  duration={lesson.duration}
+                  isCurrent={isCurrent}
+                  onPlay={() => dispatch(play({ moduleIndex, lessonIndex }))}
+                />
+              )
+            })}
         </nav>
       </Collapsible.Content>
     </Collapsible.Root>
--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -3,17 +3,27 @@ import { Header } from "../components/Header"
 import { Video } from "../components/Video"
 import { Module } from "../components/Module"
 import { useAppSelector } from "../store"
-import { useCurrentLesson } from "../store/slices/player"
+import { start, useCurrentLesson } from "../store/slices/player"
 import { useEffect } from "react"
+import { useDispatch } from "react-redux"
+import { api } from "../lib/axios"
 
 export function Player() {
-  const modules = useAppSelector((state) => state.player.course.modules)
+  const dispatch = useDispatch()
+  const modules = useAppSelector((state) => state.player.course?.modules)
 
   const { currentLesson } = useCurrentLesson()
 
   useEffect(() => {
-    document.title = `Assistindo ${currentLesson.title}`
-  }, [currentLesson.title])
+    document.title = `Assistindo: ${currentLesson?.title}`
+    api.get("/courses/1").then((response) => {
+      dispatch(start(response.data))
+    })
+  }, [])
+
+  useEffect(() => {
+    document.title = `Assistindo ${currentLesson?.title}`
+  }, [currentLesson?.title])
 
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
@@ -33,14 +43,15 @@ export function Player() {
           </div>
 
           <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-            {modules.map((module, index) => (
-              <Module
-                key={module.id}
-                moduleIndex={index}
-                title={module.title}
-                amountOfLessons={module.lessons.length}
-              />
-            ))}
+            {modules &&
+              modules.map((module, index) => (
+                <Module
+                  key={module.id}
+                  moduleIndex={index}
+                  title={module.title}
+                  amountOfLessons={module.lessons.length}
+                />
+              ))}
           </aside>
         </main>
       </div>
