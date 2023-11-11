ec9dc4665e9bc4aca3880e25899252fb19606807
jilherme <rosa.guilhermediogo@gmail.com>
2023-11-11 00:02:18 -0300

    add: Async Thunks

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,15 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
-import { player } from "./slices/player";
+import { configureStore } from "@reduxjs/toolkit"
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
+import { player } from "./slices/player"
 
 export const store = configureStore({
   reducer: {
     player,
   },
-});
+})
 
-export type RootState = ReturnType<typeof store.getState>;
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
+
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const useAppDispatch: () => AppDispatch = useDispatch
